ef873556753915ebd5026dcbecf4045eed063f19
zito-s <jhhj424@naver.com>
2022-07-12 23:46:21 +0900

    build: gradle 디펜던시 추가 설정 및 application yml 기본 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,11 @@ plugins {
 
     id("org.springframework.boot") version "2.5.14"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
     kotlin("jvm") version kotlinVersion
+    kotlin("kapt") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
+    kotlin("plugin.jpa") version kotlinVersion
 }
 
 group = "com.zito"
@@ -21,9 +24,13 @@ repositories {
 dependencies {
     val swaggerVersion = "3.0.0"
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.querydsl:querydsl-jpa")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("io.springfox:springfox-boot-starter:${swaggerVersion}")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
